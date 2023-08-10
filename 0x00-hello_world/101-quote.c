d792fe7c58b53a4f21f581124397f5776aa40a0d
MARWAMOHAMED12 <5849hamada@gmail.com>
2023-08-10 16:09:06 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <Unistd.h>
+/**
+ *main - A C program that prints a line to the standard error
+ *Return 1(Success)
+ */
+int main (void)
+{
+fput  ("and thatpieceofartisuseful\"-Dora Korpar, 2015-10-19\r", stdout) ;
+return (0);
+}
