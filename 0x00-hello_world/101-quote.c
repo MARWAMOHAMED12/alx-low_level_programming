b4bb518917f46e12a81cb5ad911849128f684ae7
MARWAMOHAMED12 <5849hamada@gmail.com>
2023-08-10 19:17:00 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,9 +2,8 @@
 /**
 *main - A C program that prints a line to the standard error
 *Return: 1(Success)
-*/
 int main (void)
 {
-Write(2, "and that piece of art is useful\"-dora Korpar,  2015-10-19\n",);
+Write(2, "and that piece of art is useful\"-dora KorparRR2015-10-19\n",);
 return (1);
 }
