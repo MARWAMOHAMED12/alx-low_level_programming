cc9865498213a3e38b7ce7bd7ff00042b7678346
MARWAMOHAMED12 <5849hamada@gmail.com>
2023-08-10 18:02:42 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
  */
 int main (void)
 {
-fput  ("and thatpieceofartisuseful\"-Dora Korpar, 2015-10-19\r", stdout) ;
-return (0);
+Write(2,"and that piece of art is useful\"-Dora Korpar, 2015-10-19\n",);
+return (1);
 }
