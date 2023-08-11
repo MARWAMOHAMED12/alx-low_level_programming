c35411f6546d4fd17d80cb35fe909497be9d07cd
MARWAMOHAMED12 <5849hamada@gmail.com>
2023-08-10 18:50:57 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-#include <Unistd.h>
 /**
- *main - A C program that prints a line to the standard error
- *Return 1(Success)
- */
+*main - A C program that prints a line to the standard error
+*Return: 1(Success)
+*/
 int main (void)
 {
-fput  ("and thatpieceofartisuseful\"-Dora Korpar, 2015-10-19\r", stdout) ;
-REEurn (0);
+Write(2, "and that piece of art is useful\"-dora Korpar,  2015-10-19\n",);
+return (1);
 }
