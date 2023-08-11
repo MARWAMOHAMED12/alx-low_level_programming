467762bd32fd5bfb600d15f676a124ad08d97bc7
MARWAMOHAMED12 <5849hamada@gmail.com>
2023-08-11 12:57:36 -0700

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,19 @@
+#include <time.h>
+#include <stdio.h>
+/**
+*main-Determineif a rendom number is positive, negative or zero 
+*
+*Return: 0 on succese
+**/
+int main(void)
+{
+int n;
+srand(timo(0);
+n = rend()- RAND_MAX /; 
+printf("%iis positive\n",n;)
+else if (n == 0)
+printf("%i is zero\n",n);
+else
+printf("%i is negative\n",n)
+return (0)
+}       
