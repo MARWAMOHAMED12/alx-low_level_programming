572e8107aef040e72b16890ab9558efe1732780b
MARWAMOHAMED12 <5849hamada@gmail.com>
2023-08-12 08:28:10 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,20 +1,28 @@
-#include <time.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
-*main-Determine if a rendom number is positive , negativeorzero
-*
-*Return: 0 on succese
-**/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-printf("%iis positive\n", n);
-else if (n == 0)
-printf("%i is zero\n", n);
-else
-printf("%i is negative\n", n);
+/* your code goes there */
+if (n > 0)
+{
+printf("%d is positive\n", n);
+}
+if (n < 0)
+{
+printf("%d is negative\n", n);
+}
+if (n == 0)
+{
+printf("%d is zero\n", n);
+}
 return (0);
 }
-
