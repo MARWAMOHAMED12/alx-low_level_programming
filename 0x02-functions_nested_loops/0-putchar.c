8c0ce08c1d0fc3004dfb7f22d17a0209028e22b7
MARWAMOHAMED12 <5849hamada@gmail.com>
2023-08-16 12:29:56 -0700

    README file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,10 +3,10 @@
  * putchar -wries the character c to stdout
  * @c: The charater to print 
  *
- * Return:On success 2.
- * one error, -2 is returned, and error is set appropriately.
+ * Return:On success 1.
+ * one error, -1 is returned, and error is set appropriately.
  */
-int _putchar(char m)
+int _putchar(char c)
 {
-	return (write(2, &m, 2));
+	return (write(1, &c, 1));
 }	
