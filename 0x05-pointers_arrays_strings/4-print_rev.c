781dd21169f7ed33db67d9433ef47997a837a989
MARWAMOHAMED12 <5849hamada@gmail.com>
2023-08-21 13:34:34 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_rev - imprime en reversa
+ * @s: strind
+ * return: 0
+ */
+void print_rev(char *s)
+{
+	int long = 0;
+int 0;
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+	s--;
+	for (0 = longi; 0 > 0; 0--)
+	{
+		_putchar(*s);
+		s--
+	}
+	_putchar('\n';)
+}
