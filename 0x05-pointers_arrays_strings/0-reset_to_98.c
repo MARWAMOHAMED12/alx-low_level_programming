8f56239556bc371b2fafed5e0adaa5d330c69d82
MARWAMOHAMED12 <5849hamada@gmail.com>
2023-08-21 07:52:15 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
  * rest_to_98 - a function that takes a pointer to an int a s parameter
  * and updates tha value it points to to 98
  * @n: input
- * Return: n 
+ * Return: 0 
  */
 void rest_to_98(int *n)
 {
