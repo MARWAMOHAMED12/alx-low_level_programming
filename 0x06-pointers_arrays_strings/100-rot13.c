f656b248e84f7162786961975b0c776e5027e27d
MARWAMOHAMED12 <5849hamada@gmail.com>
2023-08-25 13:53:00 -0700

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rot13 - encoder rot13
+ * @s: pointer to sting params
+ *
+ * Return: *s
+ */
+char *rot13(char *s)
+{
+int i;
+int m;
+char data1[] =
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char datarot[] =
+		"NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+for (i = 0; s[i] != '\0'; i++)
+{
+for (i = 0; m < 52; m++)
+{
+if (s[i] == data1[m])
+{
+s[i] = datarot[m];
+break;
+}
+}
+}
+return (s);
+}
