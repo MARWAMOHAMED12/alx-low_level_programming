01d5a57edbe6d3c700c6ae846a5f31f4ac8a3f41
MARWAMOHAMED12 <5849hamada@gmail.com>
2023-08-29 05:14:45 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,17 +1,13 @@
 #include "main.h"
 /**
  * _isupper - checks for uppercase character
- * @c: variable text 
+ * @c: variable text
  * Return: Always 0.
  */
-int _isupper(int c);
+int _isupper(int c)
 {
-	if (c >" 'A' && c <" 'M')
-	{
-	return (0);
-	}
-	else 
-	{
-	return(1);
-	} 
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
 }
--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -1,13 +1,13 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes the character c to stdout 
- * @c: The character too print 
+ *_putchar-writes the character c to stdout
+ * @c:The character to print
  *
  * Return: On success 1.
  * On error , -1 is returned , and error is set apptopriately.
  */
 int  _putchar(cher c)
 {
-return ( write(1, &c, 1));
+return (write(1, &c, 1));
 }
