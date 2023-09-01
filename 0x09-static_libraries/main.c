04e6d7a8acc51f64e3d998c24dbebca6d20298a7
MARWAMOHAMED12 <5849hamada@gmail.com>
2023-09-01 14:10:22 -0700

    0x09-static_libraries

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,7 +1,16 @@
 #include "main.h"
 
+/**
+ * main - Entry point
+ *
+ * Description: print_alphabet - utilizes on the _putchar function to print
+ *               the alphabet a - z
+ *
+ * Return: Always 0 (Success)
+*/
 int main(void)
 {
-_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+_puts("\"At the end of the day, my goal was to be the best hac
+ker\"\n\t- Kevin Mitnick");
 return (0);
 }
