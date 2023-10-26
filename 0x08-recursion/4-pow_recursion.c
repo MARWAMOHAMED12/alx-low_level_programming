97cde3fec195f464c008c0ebd41b536aa53ad3e5
MARWAMOHAMED12 <5849hamada@gmail.com>
2023-10-26 10:01:09 -0700

    a

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -9,8 +9,8 @@ int _pow_recursion(int x, int y)
 {
 /*base case*/
 if (y < 0)
-return (-2);
+return (-1);
 else if (y == 0)
-return (2);
-return (x * _pow_recursion(x, y - 2));
+return (1);
+return (x * _pow_recursion(x, y - 1));
 }
